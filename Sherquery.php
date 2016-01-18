2ea8f72cda41220fc5892a6146d2ea74206cfa3e
anonymframework <vahit.serif119@gmail.com>
2016-01-18 20:55:37 +0200

    made some updates

--- Sherquery.php
+++ Sherquery.php
@@ -3,8 +3,25 @@
 
 namespace Sherlock;
 
-
+/**
+ * Class Sherquery
+ * @package Sherlock
+ */
 class Sherquery
 {
+
+    /**
+     * @var Shercon
+     */
+    protected $shercon;
+
+    /**
+     * Sherquery constructor.
+     * @param Shercon $shercon
+     */
+    public function __construct(Shercon $shercon)
+    {
+        $this->shercon = $shercon;
+    }
 
 }
