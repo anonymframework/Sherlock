bc41a61c2d6a7ecb1ca4a8612b0cfef2bd056243
anonymframework <vahit.serif119@gmail.com>
2016-01-18 23:06:26 +0200

    made some updates

--- Rows/Limit.php
+++ Rows/Limit.php
@@ -4,9 +4,17 @@
 namespace Sherlock\Rows;
 
 
+/**
+ * Class Limit
+ * @package Sherlock\Rows
+ */
 class Limit extends Row
 {
 
+    /**
+     * Limit constructor.
+     * @param $limit
+     */
     public function __construct($limit)
     {
         $row = 'LIMIT ' . is_array($limit) ? join(',', $limit) : $limit;
